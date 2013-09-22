087eb6b190290a0e458967c511be5c7035ff20fc
goodspeedcheng <czongxiao@gmail.com>
2013-09-22 17:45:48 +0800

    添加收藏 删除 功能

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
-API_GET = "http://www.guokr.com/apis/community/basket.json"
+API_BASKET = "http://www.guokr.com/apis/community/basket.json"
+API_FRUIT = "http://www.guokr.com/apis/community/fruit.json"
 
 String.prototype.format = function() {
   var args = arguments;
@@ -14,7 +15,7 @@ function getCookies(domain, callback) {
     chrome.cookies.getAll({"domain": "www.guokr.com"}, function(cookie) {
         allCookieInfo = "";
         for (i=0; i<cookie.length; i++){
-            console.log(JSON.stringify(cookie[i]));
+            //console.log(JSON.stringify(cookie[i]));
             if (cookie[i].name == "_32353_access_token"){
                 localStorage.token = cookie[i].value;
             } else if (cookie[i].name == "_32353_ukey"){
@@ -35,24 +36,87 @@ function toHtml(data) {
 }
 
 function getBaskets(ukey) {
-    url = API_GET + '?retrieve_type=by_ukey&ukey=' + ukey;
+    url = API_BASKET + '?retrieve_type=by_ukey&ukey=' + ukey;
     $.get(url, function(data) {
         toHtml(data);
     });
 }
 
-$('.flip').click(function(){
-    $(this).find('.card').addClass('flipped').mouseleave(function(){
-        $(this).removeClass('flipped');
-    });
-    return false;
-});
+function getCurrentLink(){
+	chrome.tabs.getSelected(null, function(tab) {
+	    localStorage.current_link = tab.url;
+		localStorage.title = tab.title;
+	});
+}
+
+function addToBasket(basketId, fruit_url,title, t){
+	$.ajax({
+	    url: API_FRUIT,
+	    type: 'post',
+	    data: {
+		    retrieve_type : 'by_basket',
+	        basket_id: basketId,
+	        url: "http://www.google.com",
+	        //url: fruit_url,
+	        source: 'other_site',
+	        title: title,
+	        summary: title,
+		    access_token: localStorage.token
+		},
+	    success: function(f){
+			id = f.result.id;
+			t.data('fid', id);
+		    t.toggleClass("rotated");
+			var count = t.children(".right").text();
+			t.children(".right").text(+count + 1);
+		},
+	    dataType: 'json'
+	});
+}
+
+function hack_html(){
+	chrome.tabs.executeScript(null,
+	    {code:"document.body.style.backgroundColor='black'"});
+}
+
+function removeFromBasket(t){
+	var id = t.data('fid');
+	$.ajax({
+		url: API_FRUIT + '?' + $.param({"fruit_id": id, "access_token": localStorage.token}),
+		type: 'delete',
+		success: function(d){
+            t.toggleClass("rotated");
+			t.data('fid', '');
+			var count = t.children(".right").text();
+			t.children(".right").text(+count - 1);
+			t = null;
+		},
+		dataType: 'json'
+	});
+}
 
 $(document).ready(function(){
     getCookies();
     ukey = localStorage.ukey;
     token = localStorage.token;
     getBaskets(ukey);
+    $('.baskets').delegate('.basket','click', function(){
+		var t = $(this)
+		var bid = t.data('bid');
+		t.toggleClass("added");
+		getCurrentLink();
+		link = localStorage.current_link;
+		if (t.hasClass("added")){
+			var title = localStorage.title;
+			addToBasket(bid, link, title, t);
+		}else{
+			var id = t.data('fid');
+			removeFromBasket(t);
+		}
+        return false;
+    });
+	$('.hack').click(function(){
+		hack_html();
+	});
 });
-
 
